b3bfc7ac43f486021298dc9521b24644090e1fc7
MarceloR <marcelo.rui11@gmail.com>
2021-05-01 20:28:33 -0500

    han cho testing

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "Starting deploy"
 echo "${DOCKER_PASSWORD}" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin
-docker tag web1066-the-app $DOCKER_LOGIN/web1066-the-app:latest
+docker tag maluuba/tomcat7-java8 $DOCKER_LOGIN/web1066-the-app:latest
 docker push $DOCKER_LOGIN/web1066-the-app:latest
